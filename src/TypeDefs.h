64fc3aeb0b1448970a249f9c0a20737903675741
Bert Melis <bertmelis@users.noreply.github.com>
2022-10-07 10:39:49 +0200

    Add enum to printable conversions

--- src/TypeDefs.h
+++ src/TypeDefs.h
@@ -26,12 +26,34 @@ enum class DisconnectReason : uint8_t {
   TCP_DISCONNECTED = 7
 };
 
+static const char* disconnectReasonToString(DisconnectReason reason) {
+  switch (reason) {
+    case DisconnectReason::USER_OK:                            return "No error";
+    case DisconnectReason::MQTT_UNACCEPTABLE_PROTOCOL_VERSION: return "Unacceptable protocol version";
+    case DisconnectReason::MQTT_IDENTIFIER_REJECTED:           return "Identified rejected";
+    case DisconnectReason::MQTT_SERVER_UNAVAILABLE:            return "Server unavailable";
+    case DisconnectReason::MQTT_MALFORMED_CREDENTIALS:         return "Malformed credentials";
+    case DisconnectReason::MQTT_NOT_AUTHORIZED:                return "Not authorized";
+    case DisconnectReason::TLS_BAD_FINGERPRINT:                return "Bad fingerprint";
+    case DisconnectReason::TCP_DISCONNECTED:                   return "TCP disconnected";
+  }
+}
+
 enum class SubscribeReturncode : uint8_t {
   QOS0 = 0x00,
   QOS1 = 0x01,
   QOS2 = 0x02,
   FAIL = 0X80
 };
+
+static const char* subscribeReturncodeToString(SubscribeReturncode returnCode) {
+  switch (returnCode) {
+    case SubscribeReturncode::QOS0: return "QoS 0";
+    case SubscribeReturncode::QOS1: return "QoS 1";
+    case SubscribeReturncode::QOS2: return "QoS 2";
+    case SubscribeReturncode::FAIL: return "Failed";
+  }
+}
 
 enum class Error : uint8_t {
   SUCCESS = 0,
@@ -40,6 +62,16 @@ enum class Error : uint8_t {
   MALFORMED_PARAMETER = 3,
   MISC_ERROR = 4
 };
+
+static const char* errorToString(Error error) {
+  switch (error) {
+    case Error::SUCCESS:             return "Success";
+    case Error::OUT_OF_MEMORY:       return "Out of memory";
+    case Error::MAX_RETRIES:         return "Maximum retries exceeded";
+    case Error::MALFORMED_PARAMETER: return "Malformed parameters";
+    case Error::MISC_ERROR:          return "Misc error";
+  }
+}
 
 struct MessageProperties {
   uint8_t qos;
