2d87dbb9175ea9023960379d344254535255e863
Bert Melis <bertmelis@users.noreply.github.com>
2022-10-23 16:53:29 +0200

    Put code-to-text source in proper file

--- src/TypeDefs.h
+++ src/TypeDefs.h
@@ -26,18 +26,7 @@ enum class DisconnectReason : uint8_t {
   TCP_DISCONNECTED = 7
 };
 
-static const char* disconnectReasonToString(DisconnectReason reason) {
-  switch (reason) {
-    case DisconnectReason::USER_OK:                            return "No error";
-    case DisconnectReason::MQTT_UNACCEPTABLE_PROTOCOL_VERSION: return "Unacceptable protocol version";
-    case DisconnectReason::MQTT_IDENTIFIER_REJECTED:           return "Identified rejected";
-    case DisconnectReason::MQTT_SERVER_UNAVAILABLE:            return "Server unavailable";
-    case DisconnectReason::MQTT_MALFORMED_CREDENTIALS:         return "Malformed credentials";
-    case DisconnectReason::MQTT_NOT_AUTHORIZED:                return "Not authorized";
-    case DisconnectReason::TLS_BAD_FINGERPRINT:                return "Bad fingerprint";
-    case DisconnectReason::TCP_DISCONNECTED:                   return "TCP disconnected";
-  }
-}
+const char* disconnectReasonToString(DisconnectReason reason);
 
 enum class SubscribeReturncode : uint8_t {
   QOS0 = 0x00,
@@ -46,14 +35,7 @@ enum class SubscribeReturncode : uint8_t {
   FAIL = 0X80
 };
 
-static const char* subscribeReturncodeToString(SubscribeReturncode returnCode) {
-  switch (returnCode) {
-    case SubscribeReturncode::QOS0: return "QoS 0";
-    case SubscribeReturncode::QOS1: return "QoS 1";
-    case SubscribeReturncode::QOS2: return "QoS 2";
-    case SubscribeReturncode::FAIL: return "Failed";
-  }
-}
+const char* subscribeReturncodeToString(SubscribeReturncode returnCode);
 
 enum class Error : uint8_t {
   SUCCESS = 0,
@@ -63,15 +45,7 @@ enum class Error : uint8_t {
   MISC_ERROR = 4
 };
 
-static const char* errorToString(Error error) {
-  switch (error) {
-    case Error::SUCCESS:             return "Success";
-    case Error::OUT_OF_MEMORY:       return "Out of memory";
-    case Error::MAX_RETRIES:         return "Maximum retries exceeded";
-    case Error::MALFORMED_PARAMETER: return "Malformed parameters";
-    case Error::MISC_ERROR:          return "Misc error";
-  }
-}
+const char* errorToString(Error error);
 
 struct MessageProperties {
   uint8_t qos;
