edc00b08f9520844188522b913f165020f12882c
Bert Melis <bertmelis@users.noreply.github.com>
2023-12-19 11:57:58 +0100

    correct capitals

--- src/espMqttClientAsync.h
+++ src/espMqttClientAsync.h
@@ -23,7 +23,7 @@ class espMqttClientAsync : public MqttClientSetup<espMqttClientAsync, MQTTVERSIO
   espMqttClientAsync()
   : MqttClientSetup<espMqttClientAsync, MQTTVERSION>(espMqttClientTypes::UseInternalTask::NO)
   , _clientAsync() {
-    Mqttclient::_transport = &_clientAsync;
+    MqttClient::_transport = &_clientAsync;
     _clientAsync.client.onConnect(onConnectCb, this);
     _clientAsync.client.onDisconnect(onDisconnectCb, this);
     _clientAsync.client.onData(onDataCb, this);
