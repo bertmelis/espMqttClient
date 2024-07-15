f809fb21ceaa2ea45197252e16993c0decda396a
Bert Melis <bertmelis@users.noreply.github.com>
2024-07-15 15:07:25 +0200

    connect by hostname

--- src/Transport/ClientPosix.cpp
+++ src/Transport/ClientPosix.cpp
@@ -27,12 +27,12 @@ bool ClientPosix::connect(IPAddress ip, uint16_t port) {
 
   _sockfd = ::socket(AF_INET, SOCK_STREAM, 0);
   if (_sockfd < 0) {
-    emc_log_e("Error %d opening socket", errno);
+    emc_log_e("Error %d: \"%s\" opening socket", errno, strerror(errno));
   }
 
   int flag = 1;
   if (setsockopt(_sockfd, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(int)) < 0) {
-    emc_log_e("Error %d disabling nagle", errno);
+    emc_log_e("Error %d: \"%s\" disabling nagle", errno, strerror(errno));
   }
 
   memset(&_host, 0, sizeof(_host));
@@ -47,15 +47,70 @@ bool ClientPosix::connect(IPAddress ip, uint16_t port) {
     return false;
   }
 
-  emc_log_i("Connected");
+  emc_log_i("Socket connected");
   return true;
 }
 
 bool ClientPosix::connect(const char* host, uint16_t port) {
-  // tbi
-  (void) host;
-  (void) port;
-  return false;
+  if (connected()) stop();
+
+  int err;
+  struct addrinfo hints = {}, *addrs;
+  char port_str[16] = {};
+  
+  hints.ai_family = AF_INET; // Since your original code was using sockaddr_in and
+                             // PF_INET, I'm using AF_INET here to match.  Use
+                             // AF_UNSPEC instead if you want to allow getaddrinfo()
+                             // to find both IPv4 and IPv6 addresses for the hostname.
+                             // Just make sure the rest of your code is equally family-
+                             // agnostic when dealing with the IP addresses associated
+                             // with this connection. For instance, make sure any uses
+                             // of sockaddr_in are changed to sockaddr_storage,
+                             // and pay attention to its ss_family field, etc...
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_protocol = IPPROTO_TCP;
+
+  emc_log_i("connecting to %s:%d", host, port);
+
+  err = getaddrinfo(hostname, port_str, &hints, &addrs);
+  if (err != 0) {
+    emc_log_e("Error getting addr info: %s - %s\n", hostname, gai_strerror(err));
+    return false;
+  }
+
+  for(struct addrinfo *addr = addrs; addr != NULL; addr = addr->ai_next) {
+    _sockfd = ::socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+    if (_sockfd == -1) {
+      err = errno;
+      break; // if using AF_UNSPEC above instead of AF_INET/6 specifically,
+             // replace this 'break' with 'continue' instead, as the 'ai_family'
+             // may be different on the next iteration...
+    }
+
+    int flag = 1;
+    if (setsockopt(_sockfd, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(int)) < 0) {
+      emc_log_e("Error %d disabling nagle", errno);
+    }
+
+    if (::connect(_sockfd, addr->ai_addr, addr->ai_addrlen) == 0) {
+      break;
+    }
+
+    err = errno;
+
+    close(_sockfd);
+    _sockfd = -1;
+  }
+
+  freeaddrinfo(addrs);
+
+  if (_sockfd < 0) {
+    emc_log_e("%s: %s", hostname, strerror(err));
+    return false;
+  }
+
+  emc_log_i("Socket connected");
+  return true;
 }
 
 size_t ClientPosix::write(const uint8_t* buf, size_t size) {
